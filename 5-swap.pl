9af0cc2c91c2c860982ae4da91e791a0bf5b0c7b
Christopher <christopher@localhost.localdomain>
2015-02-11 22:06:09 -0800

    number swap

---
+++ 5-swap.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use v5.10;
+
+my $a = 5;
+my $b = 10;
+
+say "a: $a";
+say "b: $b";
+
+$a = $a ^ $b;
+$b = $b ^ $a;
+$a = $b ^ $a;
+
+say "";
+say "a: $a";
+say "b: $b";
